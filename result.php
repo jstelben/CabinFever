1102326f79652e61128acb0cf8750d5956898186
Danny Lee <dxl1720@rit.edu>
2015-05-12 03:35:16 -0400

    Updated for final display

--- result.php
+++ result.php
@@ -5,7 +5,7 @@
 		<meta charset="utf-8">
     <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Khula' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="./Styles/DefaultHeaderFooterStyle.css">
+    <link rel="stylesheet" type="text/css" href="./Styles/LoggedInStatusHeaderFooterStyle.css">
     <script type="text/javascript" src="./Assets/jquery.js"></script>
     <style type="text/css">
       main{
@@ -56,7 +56,7 @@
     <div class="HeaderFloatWrap">
       <div class="HeaderLeft">
         <div class="LogoWrap">
-          <img src="./Assets/CabinFeverLogo.png">
+          <img src="./Assets/CabinFeverLogoSmaller.png">
         </div>
         <div class="SloganWrap">
           <h1>CabinFever</h1>
@@ -65,20 +65,42 @@
       </div>
 
       <div class="HeaderRight">
-        <div class="SocialMediaRow">
-          <img src="./Assets/SocialMediaIcon-FB.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-GP.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-LI.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-P.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-RSS.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-TW.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-YT.png" class="SocialMediaIcon">
+        <div class="UpperRightFloatWrap">
+          <div class="NameDisplay">
+            <h2>User Name Goes Here</h2>
+          </div>
+          <div class="SocialMediaRow">
+            <img src="./Assets/SocialMediaIcon-FB.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-GP.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-LI.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-P.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-RSS.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-TW.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-YT.png" class="SocialMediaIcon">
+          </div>
         </div>
+      <div class="UserInterfaceTabs">
+        <div class="UITab">
+          <h2><a href="">Log Out</a></h2>
+        </div>
+        <div class="UITab">
+          <h2><a href="">Settings</a></h2>
+        </div>
+        <div class="UITab">
+          <h2><a href="">My Cabin</a></h2>
+        </div>
+        <div class="strong-tab">
+          <h2><a href="">Go Outside</a></h2>
       </div>
     </div>
-
-    <div class="HeaderAccentColorBox">
+    <div class="UserAccessRow">
+      <h3>Sign In</h3>
+      <h3 class="Register"><a href="registeruser.php">Register<a/></h3>
     </div>
+  </div>
+
+  <div class="AccentColorBox" style="float: left">
+  </div>
   </header>
 
   <main>
@@ -88,7 +110,7 @@
       </div>
       <div class='MainColumnWrap'>
         <div class='ResultColumn'>
-	  <?php 
+	  <?php
 	  	session_start();
 		if(!isset($_SESSION['rand1']))
 		{
@@ -110,7 +132,7 @@
 			$_SESSION['rand3'] = rand(1, 8);
 			while($_SESSION['rand1'] == $_SESSION['rand3'] || $_SESSION['rand2'] == $_SESSION['rand3'])
 			{
-				$_SESSION['rand3'] = rand(1, 8);	
+				$_SESSION['rand3'] = rand(1, 8);
 			}
 		}
 		$_SESSION['currentEventNum'] = $_SESSION['rand1'];
