0281c589928fd3208b2eac01b2bbe55da32235a5
Danny Lee <dxl1720@rit.edu>
2015-05-12 03:39:59 -0400

    Updated for final display

--- resulttemp.php
+++ resulttemp.php
@@ -5,22 +5,19 @@
 		<meta charset="utf-8">
     <link href='http://fonts.googleapis.com/css?family=Play' rel='stylesheet' type='text/css'>
     <link href='http://fonts.googleapis.com/css?family=Khula' rel='stylesheet' type='text/css'>
-    <link rel="stylesheet" type="text/css" href="./HeaderFooterStyle.css">
+    <link rel="stylesheet" type="text/css" href="./Styles/LoggedInStatusHeaderFooterStyle.css">
     <script type="text/javascript" src="./jquery.js"></script>
     <style type="text/css">
       main{
         width: 100%;
       }
-
         main h2, h3{
           font-family: 'Khula', sans-serif;
         }
-
         #Title{
           margin-bottom: 25px;
           text-align: center;
         }
-
         .MainWrap{
           position: relative;
           width: 900px;
@@ -29,35 +26,30 @@
           margin-right: auto;
           background-color: #FFF;
         }
-
           .MainColumnWrap{
             overflow: auto;
             overflow: hidden;
           }
-
             .ResultColumn{
               float: left;
               width: 300px;
               margin: 1em 0 1em 0;
               text-align: center;
             }
-
               .ResultColumn img{
                 margin: 0 50px 0 50px;
               }
-
         #Conclusion{
           margin-top: 25px;
           text-align: center;
         }
-
     </style>
 <body>
   <header>
     <div class="HeaderFloatWrap">
       <div class="HeaderLeft">
         <div class="LogoWrap">
-          <img src="./Assets/CabinFeverLogo.png">
+          <img src="./Assets/CabinFeverLogoSmaller.png">
         </div>
         <div class="SloganWrap">
           <h1>CabinFever</h1>
@@ -66,20 +58,42 @@
       </div>
 
       <div class="HeaderRight">
-        <div class="SocialMediaRow">
-          <img src="./Assets/SocialMediaIcon-FB.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-GP.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-LI.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-P.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-RSS.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-TW.png" class="SocialMediaIcon">
-          <img src="./Assets/SocialMediaIcon-YT.png" class="SocialMediaIcon">
+        <div class="UpperRightFloatWrap">
+          <div class="NameDisplay">
+            <h2>User Name Goes Here</h2>
+          </div>
+          <div class="SocialMediaRow">
+            <img src="./Assets/SocialMediaIcon-FB.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-GP.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-LI.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-P.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-RSS.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-TW.png" class="SocialMediaIcon">
+            <img src="./Assets/SocialMediaIcon-YT.png" class="SocialMediaIcon">
+          </div>
         </div>
+      <div class="UserInterfaceTabs">
+        <div class="UITab">
+          <h2><a href="">Log Out</a></h2>
+        </div>
+        <div class="UITab">
+          <h2><a href="">Settings</a></h2>
+        </div>
+        <div class="UITab">
+          <h2><a href="">My Cabin</a></h2>
+        </div>
+        <div class="strong-tab">
+          <h2><a href="">Go Outside</a></h2>
       </div>
     </div>
-
-    <div class="HeaderAccentColorBox">
+    <div class="UserAccessRow">
+      <h3>Sign In</h3>
+      <h3 class="Register"><a href="registeruser.php">Register<a/></h3>
     </div>
+  </div>
+
+  <div class="AccentColorBox" style="float: left">
+  </div>
   </header>
 
   <main>
@@ -89,12 +103,12 @@
       </div>
       <div class='MainColumnWrap'>
         <div class='ResultColumn'>
-	  <?php 
+	  <?php
 	  	session_start();
 	  	$_SESSION['rand1'] = 0;
 	  	$_SESSION['rand2'] = 0;
 		$_SESSION['rand3'] = 0;
-		
+
 		$_SESSION['rand1'] = rand(1, 8);
 		$_SESSION['rand2'] = rand(1, 8);
 		while($_SESSION['rand1'] == $_SESSION['rand2'])
@@ -104,9 +118,9 @@
 		$_SESSION['rand3'] = rand(1, 8);
 		while($_SESSION['rand1'] == $_SESSION['rand3'] || $_SESSION['rand2'] == $_SESSION['rand3'])
 		{
-			$_SESSION['rand3'] = rand(1, 8);	
+			$_SESSION['rand3'] = rand(1, 8);
 		}
-		
+
 		switch($_SESSION['rand1'])
 		{
 			case 1:
@@ -267,7 +281,7 @@
         <div class="ResultColumn">
         <?php
           switch($_SESSION['rand3'])
-			{	
+			{
 			case 1:
 				{
 					echo "
